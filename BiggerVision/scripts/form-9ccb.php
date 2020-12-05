9237f313a699aecd6f274dfc3e9bde1f857e9874
dlance8 <49040454+dlance8@users.noreply.github.com>
2020-12-05 17:18:57 -0500

    Add files via upload

---
+++ BiggerVision/scripts/form-9ccb.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once('FormProcessor.php');
+
+$form = array(
+    'subject' => 'New Form Submission',
+    'email_message' => 'You have a new form submission',
+    'success_redirect' => '',
+    'email' => array(
+    'from' => '',
+    'to' => 'dlance8@students.kennesaw.edu'
+    ),
+    'fields' => array(
+    'name' => array(
+    'order' => 1,
+    'type' => 'string',
+    'label' => 'Name',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Name\' is required.'
+    )
+    ),
+    'email' => array(
+    'order' => 2,
+    'type' => 'email',
+    'label' => 'Email',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Email\' is required.'
+    )
+    ),
+    'phone' => array(
+    'order' => 3,
+    'type' => 'tel',
+    'label' => 'Phone',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Phone\' is required.'
+    )
+    ),
+    'select' => array(
+    'order' => 4,
+    'type' => 'string',
+    'label' => 'select',
+    'required' => false,
+    'errors' => array(
+    'required' => 'Field \'select\' is required.'
+    )
+    ),
+    'message' => array(
+    'order' => 5,
+    'type' => 'string',
+    'label' => 'Message',
+    'required' => true,
+    'errors' => array(
+    'required' => 'Field \'Message\' is required.'
+    )
+    ),
+    )
+    );
+
+    $processor = new FormProcessor();
+    $processor->process($form);
+
+    ?>
